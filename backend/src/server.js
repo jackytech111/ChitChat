fcf5e5b8fdbc15c980e727ef656a7eabb0d2c847
Jacky <khainguyenviet2000@gmail.com>
2026-03-01 01:31:44 +0700

    Add Swagger JSON

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ import conversationRoute from "./routes/conversationRoute.js";
 import authRoute from "./routes/authRoute.js";
 import { protectedRoute } from "./middlewares/authMiddleware.js";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
 
 dotenv.config();
 
@@ -20,6 +22,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
+// swagger
+const swaggerDocument = JSON.parse(
+  fs.readFileSync("./src/swagger.json", "utf8"),
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // public routes
 app.use("/api/auth", authRoute);
 
